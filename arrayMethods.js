35a4c99f5f2e98299c28d4cb04e21964df2cd019
fazza-tech <ebranfazza@gmail.com>
2024-12-14 08:59:27 +0530

    map completed

---
+++ arrayMethods.js
@@ -0,0 +1,16 @@
+const arr = [5,1,3,2,6];
+
+function double(x){
+    return x *2;
+}
+function triple(x){
+    return x *3;
+}
+
+
+
+const output = arr.map(  (x) => x.toString(2)); //binary convertion
+
+
+
+console.log(output);
